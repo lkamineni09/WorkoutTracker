3edf93550520c41f58c05c34a8147f124b73abfb
Lasya Kamineni <lkamineni09@gmail.com>
2024-07-28 16:42:15 -0500

    Setup

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainPage from './components/MainPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>LASYA</Text>
       <StatusBar style="auto" />
+      {/* <MainPage></MainPage> */}
     </View>
   );
 }
