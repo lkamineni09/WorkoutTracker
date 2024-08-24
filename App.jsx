6eba90a274fe28a0c2644a702fe75336e0bd709a
Yatindra Kamineni <lasyayatin@gmail.com>
2024-08-24 09:30:46 -0500

    Created login page

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainPage from './components/MainPage';
+import LoginPage from './components/LoginPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <MainPage></MainPage>
+      <LoginPage></LoginPage>
     </View>
   );
 }
